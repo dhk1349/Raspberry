3dcb4c8ae3bc2e497299877778e5cc7b8cabcd30
dhk1349 <dhk1349@naver.com>
2019-09-26 10:09:21 +0900

    Create CES437_3_2.py

---
+++ CES437_3_2.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 09:52:44 2019
+
+@author: dhk13
+"""
+
+import RPi.GPIO as GPIO
+
+import time
+
+REDLEDlist=[
+        4, 17, 18, 27, 22, 23, 24, 25
+        ]
+
+GREENLEDlist=[
+         6, 12, 13, 16, 19, 20, 26, 21
+        ]
+
+def LEDControl(color):
+    if color=="RED":
+        for i in range(8):
+            GPIO.output(REDLEDlist[i], GPIO.HIGH)
+            
+            GPIO.output(GREENLEDlist[i], GPIO.LOW)
+    elif color=="GREEN":
+        for i in range(8):
+            GPIO.output(REDLEDlist[i], GPIO.LOW)
+            
+            GPIO.output(GREENLEDlist[i], GPIO.HIGH)
+            
+GPIO.setmode(GPIO.BCM)
+    
+for i in REDLEDlist:
+    GPIO.setup(i, GPIO.OUT, inintial=GPIO.LOW)
+for i in GREENLEDlist:
+    GPIO.setup(i, GPIO.OUT, initial-GPIO.LOW)
+    
+time.sleep(0.5)
+
+for i in range(10):
+    try:
+        LEDControl("RED")
+        time.sleep(0.5)
+        LEDControl("GREEN")
+    except KeyboardInterrupt:
+        pass
+GPIO.cleanup()
+    
+    
+    
+    
+    
+    
