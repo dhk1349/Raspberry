52c6f350c04cb3160e6369f27a737a3ec799a9f6
dhk1349 <dhk1349@naver.com>
2019-09-26 10:54:43 +0900

    Update CSE437_3_4.py

--- CSE437_3_4.py
+++ CSE437_3_4.py
@@ -9,4 +9,24 @@ import RPi.GPIO as GPIO
 
 import time
 
-LEDlist=[]
+LEDlist=[
+         4, 17, 18, 27, 22, 23, 24, 25,
+         6, 12, 13, 16, 19, 20, 26, 21
+        ]
+
+GPIO.setmode(GPIO.BCM)
+
+for i in LEDlist:
+    GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
+    
+for i in range(0,len(LEDlist), 2):
+    GPIO.OUT(LEDlist[i], GPIO.HIGH)
+    GPIO.OUT(LEDlist[i+1], GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.OUT(LEDlist[i], GPIO.LOW)
+    GPIO.OUT(LEDlist[i+1], GPIO.LOW)    
+    time.sleep(0.5)
+    
+
+
+GPIO.cleanup()
