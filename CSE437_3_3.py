1a616b82d0e2a6165ed4184c47c2b95097dbbb57
dhk1349 <dhk1349@naver.com>
2019-09-26 10:48:52 +0900

    .

--- CSE437_3_3.py
+++ CSE437_3_3.py
@@ -4,4 +4,36 @@ Created on Thu Sep 26 10:10:40 2019
 
 @author: dhk13
 """
+import RPi.GPIO as GPIO
 
+import time
+
+LEDlist=[
+         4, 17, 18, 27, 22, 23, 24, 25,
+         6, 12, 13, 16, 19, 20, 26, 21
+        ]
+
+LEDeven=[
+        17, 27, 23, 25, 12, 16, 20, 21
+        ]
+LEDodd=[
+        4, 18, 22, 24, 6, 13, 19, 26 
+        ]
+
+GPIO.setmode(GPIO.BCM)
+
+for i in LEDlist:
+    GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
+
+for i in LEDodd:
+    GPIO.output(i, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(i, GPIO.LOW)
+    time.sleep(0.5)
+for i in LEDeven:
+    GPIO.output(i, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(i, GPIO.LOW)
+    time.sleep(0.5)
+
+GPIO.cleanup()
--- CSE437_3_4.py
+++ CSE437_3_4.py
@@ -5,3 +5,8 @@ Created on Thu Sep 26 10:37:44 2019
 @author: dhk13
 """
 
+import RPi.GPIO as GPIO
+
+import time
+
+LEDlist=[]
