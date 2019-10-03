dad0069ecd5e237c51d22ebf3eec1e3212791126
Donghoon Han <dhk1349@naver.com>
2019-10-03 11:13:02 +0900

    .

---
+++ CSE437_5_1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 10:46:02 2019
+
+@author: donghoon
+"""
+
+import RPi.GPIO as GPIO
+
+import time
+
+usleep=lambda x: time.sleep(x/1000000.0)
+
+import datetime as dt
+
+TP=4
+EP=17
+
+def getDistance():
+    fDistance = 0.0
+    nStartTime, nEndTime=0.0
+    
+    GPIO.output(TP, GPIO.LOW)
+    usleep(2)
+    
+    GPIO.output(TP, GPIO.HIGH)
+    usleep(10)
+    
+    GPIO.output(TP, GPIO.LOW)
+    
+    while(GPIO.input(EP)==GPIO.LOW):
+        pass
+    nStartTime = dt.datetime.now()
+    while(GPIO.input(EP)==GPIO.HIGH):
+        pass
+    nEndTime = dt.datetime.now()    
